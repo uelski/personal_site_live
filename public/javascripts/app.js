7d75c4c21153044a9c0aae1b457e3fb54f72d58f
Sam Vredenburgh <sam.vburgh@gmail.com>
2016-02-22 17:09:38 -0600

    added contact at bottom

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,4 +2,23 @@ $(document).ready(function() {
 
 console.log('connected');
 
+$("#about_button").click(function() {
+  scrollTime('about')
+})
+
+$("#projects_button").click(function() {
+  scrollTime('show-up')
+})
+
+$("#contact_button").click(function() {
+  scrollTime('footer')
+})
+
+
+
 })//end of document.ready
+
+function scrollTime(did) {
+  var aTag = $("div[id='" + did + "']");
+  $('html,body').animate({scrollTop: aTag.offset().top}, 'slow');
+}
